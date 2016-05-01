a50bdcf917f3106a4deaf46c64f3b78347ce7f80
Lauren Eisdorfer <leisdorfer67@gmail.com>
2016-05-01 13:51:56 -0500

    Updated save

---
+++ Locals.swift
@@ -0,0 +1,13 @@
+//
+//  Locals.swift
+//  LocalRetreat
+//
+//  Created by Lauren Eisdorfer on 4/30/16.
+//  Copyright © 2016 Lauren Eisdorfer. All rights reserved.
+//
+
+import Foundation
+
+struct LocalsInformation{
+    let locals: [String: Dictionary<String, String>] = ["Jerry":["City": "Seattle", "Preference": "Adventurer"], "Beth":["City": "Seattle", "Preference": "Foodie"], "Emily":["City": "Boston", "Preference": "History Buff"], "Eric":["City": "Boston", "Preference": "Art Design Lover"], "Paul":["City": "Chicago", "Preference": "Cultural Explorer"], "Leslie":["City":"Chicago", "Preference":"Night Owl"], "Rachel":["City": "San Francisco", "Preference": "Foodie"], "Kavon":["City":"San Francisco", "Preference":"Adventurer"], "Lauren":["City":"New York", "Preference": "History Buff"], "Ben":["City":"New York", "Preference":"Art Design Lover"], "Rebecca":["City":"Austin","Preference": "Cultural Explorer"], "Susie":["City": "Austin", "Preference" :"NightOwl"] ]
+}
---
+++ Traveller.swift
@@ -0,0 +1,47 @@
+//
+//  Traveller.swift
+//  LocalRetreat
+//
+//  Created by Lauren Eisdorfer on 4/30/16.
+//  Copyright © 2016 Lauren Eisdorfer. All rights reserved.
+//
+
+import Foundation
+
+
+protocol TravellerType{
+    var destination: Destination {get}
+    var travellingPreference: TravellingPreference {get}
+    init(destination: Destination, travellingPreference: TravellingPreference)
+}
+
+enum Destination: String {
+    case NewYork = "New York"
+    case Seattle = "Seattle"
+    case SanFrancisco = "San Francsico"
+    case Boston = "Boston"
+    case Austin = "Austin"
+    case Chicago = "Chicago"
+}
+
+enum TravellingPreference: String{
+    case Adventurer = "Adventurer"
+    case ArtDesignLover = "Art Design Lover"
+    case Foodie = "Foodie"
+    case CulturalExplorer = "Cultural Explorer"
+    case HistoryBuff = "History Buff"
+    case NightOwl = "Night Owl"
+}
+
+enum Errors: ErrorType {
+    case InvalidPath
+}
+
+class Traveller: TravellerType{
+   var destination: Destination
+   var travellingPreference: TravellingPreference
+    required init(destination: Destination, travellingPreference: TravellingPreference){
+        self.destination = destination
+        self.travellingPreference = travellingPreference
+    }
+}
